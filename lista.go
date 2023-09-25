945c8b9d4c79d1e8adc4a56c1650eea5eb809b9e
Cande Matelica <candela.matelica@gmail.com>
2023-09-25 17:26:21 -0300

    Se agrega primera muestra de lista

---
+++ lista.go
@@ -0,0 +1,20 @@
+type Lista[T any] interface {
+	//Esta vacia devuelve verdadero si la lista no contiene ningun elemento, falso en caso contario.
+	EstaVacia() bool
+	// insertar primero, inserta el elemento pasado por parametro al inicio de la lista
+	InsertarPrimero(T)
+	//insertar ultimo, inserta el elemento pasado por parametro al final de la lista
+	InsertarUltimo(T)
+	//borrar primero, borra el primer elemento de la lista, devuelve un panic si esta vacia
+	BorrarPrimero() T
+	//ver primero, muestra el primer elemento de la lista, devuelve un panic si esta vacia
+	VerPrimero() T
+	//ver ultimo, muestra el primer elemento de la lista, devuelve un panic si esta vacia
+	VerUltimo() T
+	//ver largo devuelve la cantidad de elementos que tiene la lista
+	Largo() int
+	//iterador interno
+	Iterar(visitar func(T) bool)
+	//iterador externo
+	Iterador() IteradorLista[T]
+}
