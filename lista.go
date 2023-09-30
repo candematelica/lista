4db78d1b96b92a038d94bf0994bb01ac4fc6ba4a
jesapugliese <129856283+jesapugliese@users.noreply.github.com>
2023-09-30 16:15:42 -0300

    lista.go

--- lista.go
+++ lista.go
@@ -21,5 +21,19 @@ type Lista[T any] interface {
 	Iterador() IteradorLista[T]
 }
 
-
-
+type IteradorLista[T any] interface {
+	//'VerActual' devuelve el elemento sobre el cual estamos parados en la lista, en caso de que
+	//se llame a esta funcion cuando el iterador ya haya terminado de iterar, devolvera un panic
+	VerActual() T
+	//'HaySiguiente' devuelve true si se puede seguir iterando (es decir si hay elementos despues
+	//del cual estemos parados). En caso contrario devuelve false
+	HaySiguiente() bool
+	//'Siguiente' devuelve el siguiente elemento a la posicion en la que estamos parados en la lista, en caso
+	//de que se llame a esta funcion cuando el iterador ya haya terminado de iterar, devolvera un panic
+	Siguiente() T
+	//'Insertar' inserta un elemento pasado por parametro en la posicion en la que estamos parados en la lista
+	Insertar(T)
+	//'Borrar' borra y devuelve el elemento sobre el cual estemos parados en la lista, en caso de que
+	//se llame a esta funcion cuando el iterador ya haya terminado de iterar, devolvera un panic
+	Borrar() T
+}
