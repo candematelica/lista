335cec85b77cb0da412d5520fe54be072e1002f6
jesapugliese <129856283+jesapugliese@users.noreply.github.com>
2023-09-30 15:58:20 -0300

    lista_test.go

---
+++ lista_test.go
@@ -0,0 +1,31 @@
+package lista_test
+
+import (
+	"lista"
+	"testing"
+  
+	"github.com/stretchr/testify/require"
+}
+
+func TestListaVacia(t *testing.T) {
+	
+}
+
+func TestInsertarYBorrarVariosElementosNumericos(t *testing.T) {
+	//En este la idea seria probar insertar y borrar sin que la lista llegue a estar vacía
+  //Probar acá funciones como el Largo, VerPrimero y VerUltimo
+}
+
+func TestInsertarYBorrarVariosElementosDeTipoString(t *testing.T) {
+	//En este la idea seria probar insertar y borrar sin que la lista llegue a estar vacía
+  //Probar acá funciones como el Largo, VerPrimero y VerUltimo
+}
+
+func TestListaConUnElemento(t *testing.T) {
+  //Aca probamos que al borrar una lista con un elemento esta se comporte como vacia	
+}
+
+func TestVolumen(t *testing.T) {
+	//Usar los iteradores?
+  //Hacer uno usando BorrarPrimero y InsertarPrimero y otro usando BorrarPrimero y InsertarUltimo
+}
