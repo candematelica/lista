a4dd06cba08a36c0153b2aaa1813e1ae328511ff
jesapugliese <129856283+jesapugliese@users.noreply.github.com>
2023-09-30 15:59:23 -0300

    lista_test.go

--- lista_test.go
+++ lista_test.go
@@ -13,19 +13,19 @@ func TestListaVacia(t *testing.T) {
 
 func TestInsertarYBorrarVariosElementosNumericos(t *testing.T) {
 	//En este la idea seria probar insertar y borrar sin que la lista llegue a estar vacía
-  //Probar acá funciones como el Largo, VerPrimero y VerUltimo
+  	//Probar acá funciones como el Largo, VerPrimero y VerUltimo
 }
 
 func TestInsertarYBorrarVariosElementosDeTipoString(t *testing.T) {
 	//En este la idea seria probar insertar y borrar sin que la lista llegue a estar vacía
-  //Probar acá funciones como el Largo, VerPrimero y VerUltimo
+  	//Probar acá funciones como el Largo, VerPrimero y VerUltimo
 }
 
 func TestListaConUnElemento(t *testing.T) {
-  //Aca probamos que al borrar una lista con un elemento esta se comporte como vacia	
+  	//Aca probamos que al borrar una lista con un elemento esta se comporte como vacia	
 }
 
 func TestVolumen(t *testing.T) {
 	//Usar los iteradores?
-  //Hacer uno usando BorrarPrimero y InsertarPrimero y otro usando BorrarPrimero y InsertarUltimo
+  	//Hacer uno usando BorrarPrimero y InsertarPrimero y otro usando BorrarPrimero y InsertarUltimo
 }
