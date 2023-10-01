34009da26100ddd1d4924ba4869ccdea7bcc59f0
candematelica <candela.matelica@gmail.com>
2023-10-01 01:03:33 -0300

    ultimos cambios realizados en la madrugada anterior al gran superclasico :)

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -12,25 +12,25 @@ type listaEnlazada[T any] struct {
 }
 
 type iteradorLista[T any] struct {
-	lista *listaEnlazada[T]
+	lista  *listaEnlazada[T]
 	actual *nodoLista[T]
 }
 
-func CrearListaEnlazada[T any]() Lista[T]{
-	return &listaEnlazada{}
+func CrearListaEnlazada[T any]() Lista[T] {
+	return &listaEnlazada[T]{primero: nil, ultimo: nil, largo: 0}
 }
 
-func crearNodo[T any](dato T) *nodoLista[T]{
-	return &nodoLIsta[T]{dato: dato}
+func crearNodo[T any](dato T) *nodoLista[T] {
+	return &nodoLista[T]{dato: dato}
 }
 
-func (lista listaEnlazada[T])[T any]EstaVacia() bool {
-	return lista.primero == nil 
+func (lista listaEnlazada[T]) EstaVacia() bool {
+	return lista.primero == nil
 }
 
-func (lista *listaEnlazada[T])InsertarPrimero[T any](elem T){
+func (lista *listaEnlazada[T]) InsertarPrimero(elem T) {
 	nodoNuevo := crearNodo[T](elem)
-	if lista.EstaVacia(){
+	if lista.EstaVacia() {
 		lista.ultimo = nodoNuevo
 	} else {
 		nodoNuevo.siguiente = lista.primero
@@ -40,63 +40,60 @@ func (lista *listaEnlazada[T])InsertarPrimero[T any](elem T){
 	lista.largo++
 }
 
-func (lista *listaEnlazada[T])InsertarUltimo[T any](elem T){
+func (lista *listaEnlazada[T]) InsertarUltimo(elem T) {
 	nodoNuevo := crearNodo[T](elem)
 
-	if lista.EstaVacia(){
+	if lista.EstaVacia() {
 		lista.primero = nodoNuevo
 	}
 
-	//Acá arriba no habría que agregar:
-	//lista.ultimo := ante_ultimo
-	//ante_ultimo.siguiente = nodoNuevo
-	//??
+	lista.ultimo.siguiente = lista.ultimo
 	lista.ultimo = nodoNuevo
-	lista.largo ++
+	lista.largo++
 }
 
-func chequearListaVacia[T any](lista listaEnlazada[T]) {
+func chequearListaVacia[T any](lista *listaEnlazada[T]) {
 	if lista.EstaVacia() {
 		panic("La lista esta vacia")
 	}
 }
 
-func (lista *listaEnlazada[T])BorrarPrimero[T any]() T{
+func (lista *listaEnlazada[T]) BorrarPrimero() T {
 
-	chequearListaVacia[T](lista)
+	chequearListaVacia(lista)
 
 	dato := lista.primero.dato
 
 	lista.primero = lista.primero.siguiente
 
-	lista.largo --
+	lista.largo--
 
-	if lista.largo == 0{
-		lista.ultimo == nil
+	if lista.largo == 0 {
+		lista.ultimo = nil
 	}
 
 	return dato
 
 }
 
-func (lista listaEnlazada[T])VerPrimero[T any]() T{
+func (lista *listaEnlazada[T]) VerPrimero() T {
 	chequearListaVacia[T](lista)
 	return lista.primero.dato
 }
 
-func (lista listaEnlazada[T])VerUlitmo[T any]() T{
+func (lista *listaEnlazada[T]) VerUlitmo() T {
 	chequearListaVacia[T](lista)
 	return lista.ultimo.dato
 }
 
-func (lista listaEnlazada[T])Largo[T any]() int{
+func (lista *listaEnlazada[T]) Largo() int {
 	return lista.largo
 }
 
-func (lista listaEnlazada[T])Iterar[T any](visitar func(T) bool){
+func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
 	nodoActual := lista.primero
 
-	for nodoActual != nil{
+	for nodoActual != nil {
 		valor := nodoActual.dato
 		visitar(valor)
 		nodoActual = nodoActual.siguiente
@@ -104,30 +101,30 @@ func (lista listaEnlazada[T])Iterar[T any](visitar func(T) bool){
 }
 
 func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
-	return &iteradorLista[T]{lista, lista.VerPrimero()}
+	return iteradorLista[T]{lista: lista, actual: lista.primero}
 }
 
-func (iter iteradorLista[T]) chequearIterador() {
+func (iter *iteradorLista[T]) chequearIterador() {
 	if !iter.HaySiguiente() {
 		panic("El iterador ya itero")
 	}
 }
 
-func (iter iteradorLista[T]) VerActual() T {
+func (iter *iteradorLista[T]) VerActual() T {
 	iter.chequearIterador()
 
 	return iter.actual.dato
 }
 
-func (iter iteradorLista[T]) HaySiguiente() bool {
-	return iter.actual.siguiente != nil
+func (iter *iteradorLista[T]) HaySiguiente() bool {
+	return iter.actual != nil
 }
 
-func (iter *iteradorLista[T]) Siguiente() T {
+func (iter *iteradorLista[T]) Siguiente() {
 	iter.chequearIterador()
-	
+
 	iter.actual = iter.actual.siguiente
-	return iter.actual.dato
+
 }
 
 func (iter *iteradorLista[T]) Insertar(elem T) {
@@ -137,9 +134,9 @@ func (iter *iteradorLista[T]) Insertar(elem T) {
 		iter.actual.siguiente = nil
 		puntero_ante_ultimo.siguiente = iter.actual
 	} else {
-		puntero_siguiente := iter.actual
+		iter.actual.siguiente = iter.actual
 		iter.actual.dato = elem
-		iter.actual.siguiente = puntero_siguiente
+
 	}
 }
 
@@ -148,7 +145,6 @@ func (iter *iteradorLista[T]) Borrar() T {
 
 	dato := iter.actual.dato
 	if !iter.HaySiguiente() {
-		iter.actual.dato = nil
 		iter.actual = iter.lista.ultimo
 	} else {
 		iter.actual.dato = iter.actual.siguiente.dato
--- lista_test.go
+++ lista_test.go
@@ -3,16 +3,16 @@ package lista_test
 import (
 	TDALista "tdas/lista"
 	"testing"
-  
+
 	"github.com/stretchr/testify/require"
 )
 
-const int granTamanio = 10000
+const int GRANTAMANIO = 10000
 
 func TestListaVacia(t *testing.T) {
 	lista := TDALista.CrearListaEnlazada[int]()
 	require.True(t, lista.EstaVacia())
-	require.EqualValues(t, 0, lista.Largo(), "El largo de una pila vacia es 0")
+	require.EqualValues(t, 0, lista.Largo(), "El largo de una lista vacia es 0")
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() }, "Hay un panic al querer borrar el primer elemento de una lista vacia")
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() }, "Hay un panic al querer ver el primer elemento de una lista vacia")
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() }, "Hay un panic al querer ver el ultimo elemento de una lista vacia")
@@ -156,18 +156,18 @@ func TestIteradorInterno(t *testing.T) {
 	require.EqualValues(t, 11, suma, "Se puede iterar toda la lista correctamente con el iterador interno")
 }
 
-func TestVolumen(t *testing.T) {
+func TestVolumenPrimero(t *testing.T) {
 	//Ver, no se si esta bien este o.o
 	lista := TDALista.CrearListaEnlazada[int]()
 
-	for i := 0; i < granTamanio; i++ {
+	for i := 0; i < GRANTAMANIO; i++ {
 		lista.InsertarPrimero(i)
 	}
 	require.False(t, lista.EstaVacia())
 	require.EqualValues(t, 9999, lista.VerPrimero())
 	require.EqualValues(t, 0, lista.VerUltimo())
-	require.EqualValues(t, granTamanio, lista.Largo())
-	for i := granTamanio - 1; i > 0; i-- {
+	require.EqualValues(t, GRANTAMANIO, lista.Largo())
+	for i := GRANTAMANIO - 1; i > 0; i-- {
 		require.EqualValues(t, i, lista.VerPrimero())
 		require.EqualValues(t, 0, lista.VerUltimo())
 		require.EqualValues(t, i+1, lista.Largo())
@@ -178,18 +178,21 @@ func TestVolumen(t *testing.T) {
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.BorrarPrimero() })
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerPrimero() })
 	require.PanicsWithValue(t, "La lista esta vacia", func() { lista.VerUltimo() })
+}
+func TestVolumenUltimo(t *testing.T) {
+	lista := TDALista.CrearListaEnlazada[int]()
 
-	for i := 0; i < granTamanio; i++ {
+	for i := 0; i < GRANTAMANIO; i++ {
 		lista.InsertarUltimo(i)
 	}
 	require.False(t, lista.EstaVacia())
 	require.EqualValues(t, 0, lista.VerPrimero())
 	require.EqualValues(t, 9999, lista.VerUltimo())
-	require.EqualValues(t, granTamanio, lista.Largo())
-	for i := 0; i < granTamanio; i++ {
+	require.EqualValues(t, GRANTAMANIO, lista.Largo())
+	for i := 0; i < GRANTAMANIO; i++ {
 		require.EqualValues(t, i, lista.VerPrimero())
 		require.EqualValues(t, 9999, lista.VerUltimo())
-		require.EqualValues(t, granTamanio-i, lista.Largo())
+		require.EqualValues(t, GRANTAMANIO-i, lista.Largo())
 		lista.BorrarPrimero()
 	}
 	require.True(t, lista.EstaVacia())
